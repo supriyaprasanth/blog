99150d24e8de29fd1c471dd116d885c0d0c4f2bf
supriyaprasanth <supriya.p@inapp.com>
2018-08-02 12:55:51 +0530

    intial commit

---
+++ myproject/blog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import models
+from .filters import PostFilter
+
+@login_required
+def post_list(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+@login_required
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user    
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_remove(request, pk):  
+    post = get_object_or_404(Post, pk=pk)
+    if request.user == post.author:
+        post.delete()
+    return redirect('post_list') 
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def search(request):
+    postt_list = Post.objects.all()
+    postt_filter = UserFilter(request.GET, queryset=user_list)
+    return render(request, 'search/user_list.html', {'filter': user_filter})
+
+def search(request):
+    postt_list = Post.objects.all()
+    post_filter = PostFilter(request.GET, queryset=postt_list)
+    return render(request, 'blog/search_list.html', {'filter': post_filter})
